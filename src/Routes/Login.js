a86ed840bc8d548b56ab5daf79b27bdf9bba8446
El-zabeth <134275751+El-zabeth@users.noreply.github.com>
2023-06-18 11:06:01 +0530

    Login.js

--- src/Routes/Login.js
+++ src/Routes/Login.js
@@ -1,23 +1,43 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Perform login logic here
-    console.log("Login button clicked");
-    console.log("Email:", email);
-    console.log("Password:", password);
+    setError(""); // Reset error message
+
+    try {
+      const response = await axios.post("/api/login", {
+        email,
+        password,
+      });
+
+      // Assuming login is successful
+      console.log("Login successful");
+      console.log("User data:", response.data);
+
+      // Navigate to the home page
+      // window.location = "/home";
+    } catch (error) {
+      // Handle error
+      console.log("Login failed");
+      console.error(error.response.data);
+      setError("Invalid email or password. Please try again.");
+    }
   };
 
   return (
     <div className="login-container">
       <div className="login-box">
         <form className="login-form" onSubmit={handleSubmit}>
-          <h2 className="login-title">Login</h2> <br />
+          <h2 className="login-title">Login</h2>
+          <br />
           <br />
           <input
             className="login-input"
@@ -36,8 +56,9 @@ const Login = () => {
           <button className="login-button" type="submit">
             Login
           </button>
-           <p className="login-signup">
-            Don't have an account? <a href="/s">Sign Up</a>
+          {error && <p className="login-error">{error}</p>}
+          <p className="login-signup">
+            Don't have an account? <Link to="/s">Sign Up</Link>
           </p>
         </form>
       </div>
